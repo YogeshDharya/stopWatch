997724eed20e832c632ef8f4d08a45ef9ebb4107
YogeshDharya <yogeshdharya@gmail.com>
2024-05-27 16:36:01 +0530

    startWatch

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [timer, setTimer] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  const handleStartorStop = () => {
+    setIsRunning((prevIsRunning) => !prevIsRunning);
+  };
+
+  const formatTime = (timeInSeconds) => {
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = timeInSeconds % 60;
+    return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+  };
+  useEffect(() => {
+    let intervalID;
+
+    if (isRunning) {
+      intervalID = setInterval(() => {
+        setTimer((prevTimer) => prevTimer + 1);
+      }, 1000);
+    }
+
+    return () => clearInterval(intervalID);
+  }, [isRunning]);
+
+
+  const handleRestart=()=>{
+    setTimer(0);
+  }
+  return (
+    <>
+    <h1>Stopwatch</h1>
+      <h3>time:{formatTime(timer)}</h3>
+      <button onClick={handleStartorStop}>{isRunning ? 'Stop' : 'Start'}</button>
+      <button onClick={handleRestart}>Reset</button>
+    </>
+  );
+}
+
+export default App;
+
+
